a9f4ca108aae7d68b641b79fb6596ca2012e4b18
Brad Hoffman <Brad Hoffman@128.4.87.69>
2017-02-26 23:28:36 -0500

    Quiz 2: Question 1

---
+++ src/tuitionCalculator/TuitionCalc.java
@@ -0,0 +1,27 @@
+package tuitionCalculator;
+
+import java.util.Scanner;
+
+public class TuitionCalc {
+	public void Tuition() {
+
+		java.util.Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your initial tuition cost");
+		double ituition = input.nextDouble();
+		
+		System.out.println("By what percentage will your tuition increase");
+		double ipercentage = 1 + ((input.nextDouble()) / 100);
+		
+		double total = 0;
+		for (int n = 0; n < 4; n++){
+			
+			double yearlytotal = ituition * Math.pow(ipercentage, n);
+			ituition = yearlytotal;
+			total += yearlytotal;
+			input.close();
+		}
+		
+		System.out.printf("The total tuition is" + total);
+	}
+}
